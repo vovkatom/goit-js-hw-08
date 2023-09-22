64ee83833302d0324a51bf527b6d24e8f37a6852
vovkatom <36505066+vovkatom@users.noreply.github.com>
2023-09-22 18:22:52 +0000

    Deploying to gh-pages from @ vovkatom/goit-js-hw-08@4133bd8c93b109f1a17edc0f5f7422e136b8d521 🚀

---
+++ 03-feedback.a41825d7.js
@@ -0,0 +1,4 @@
+const e=document.querySelector(".feedback-form"),a=e.querySelector('[name="email"]'),t=e.querySelector('[name="message"]'),l=()=>{let e={email:a.value,message:t.value};localStorage.setItem("feedback-form-state",JSON.stringify(e))};a.addEventListener("input",l),t.addEventListener("input",l),window.addEventListener("load",()=>{let e=JSON.parse(localStorage.getItem("feedback-form-state"))||{};a.value=e.email||"",t.value=e.message||""}),e.addEventListener("submit",e=>{e.preventDefault();let l={email:a.value,message:t.value};console.log(l),a.value="",t.value="",// Видаляємо дані з локального сховища
+localStorage.removeItem("feedback-form-state")});//# sourceMappingURL=03-feedback.a41825d7.js.map
+
+//# sourceMappingURL=03-feedback.a41825d7.js.map
